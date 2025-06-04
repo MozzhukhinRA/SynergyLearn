41c217b7db38ac199e8df4acd12d5e90b050e1e0
MozzhukhinRA <roma.moz@mail.ru>
2025-06-04 23:13:17 +0300

    New Folder for filter

--- Python.Syn/Task2.py
+++
@@ -1,6 +0,0 @@
-print("Pleas write all stage evolution human")
-st1,st2,st3,st4,st5,st6 =  input(),input(),input(),input(),input(),input()
-if st6 == "Homo sapience":
-    print(st1,st2,st3,st4,st5,st6,sep=" => ")
-else:
-    print("Pls, try again")
