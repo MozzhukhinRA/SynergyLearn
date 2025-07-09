ef93f7ef72b640f5ad5f5d81c4bd7c7d79e4873d
MozzhukhinRA <roma.moz@mail.ru>
2025-07-09 23:41:45 +0300

    New HW

--- GameStudy14/map.py
+++ GameStudy14/map.py
@@ -1,3 +1,7 @@
+from util import randbool
+from util import randcell
+from util import randcell2
+
 #0 - поле
 #1 - дерево
 #2 -река
@@ -8,9 +12,24 @@ CELL_TYPES = "🟩🌳🌊🏥🏠"
 
 class Map:
 
-    #def generate_rivers():
+    def generate_rivers(self, l):
+        rc = randcell(self.w, self.h)
+        rx,ry = rc[0], rc[1]
+        self.cells[rx][ry] = 2
+        while l > 0:
+            rc2 = randcell2(rx,ry)
+            rx2,ry2 = rc2[0],rc2[1]
+            if self.check_bounds(rx2,ry2):
+                self.cells[rx2][ry2] = 2
+                rx,ry = rx2, ry2
+                l -= 1
 
-    #def generate_firest():
+    def generate_forest(self, r, mxr):
+        for ri in range(self.h):
+            for ci in range(self.w):
+                if randbool(r, mxr):
+                    self.cells[ri][ci] = 1
+
 
     def print_map(self):
         print("⬛" * (self.w + 2))
@@ -32,11 +51,9 @@ class Map:
         self.h = h
         self.cells = [[0 for i in range(w)] for j in range(h)]
 
-battle = Map(30,20)
-battle.cells[1][2] = 1
-battle.cells[2][4] = 2
-battle.cells[4][4] = 3
-battle.cells[5][5] = 4
-if battle.check_bounds(20,9):
-    print("YES")
+battle = Map(20,20)
+battle.generate_forest(3, 10)
+battle.generate_rivers(10)
+battle.generate_rivers(40)
+battle.generate_rivers(5)
 battle.print_map()
